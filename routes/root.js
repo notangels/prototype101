b82be2b9ed23b2a7a921e0dd0cf41b823316235e
notangels <43252341+notangels@users.noreply.github.com>
2024-05-08 21:30:30 -0500

    Update root.js

--- routes/root.js
+++ routes/root.js
@@ -3,16 +3,7 @@ const router = express.Router();
 const statesRouter = require('./api/states');
 
 
-router.use('/states', statesRouter);
 
-// Catch-all route for 404 errors
-router.use((req, res) => {
-  const contentType = req.accepts('html', 'json');
-  if (contentType === 'html') {
-    res.status(404).send('<h1>404 Not Found</h1>');
-  } else {
-    res.status(404).json({ error: '404 Not Found' });
-  }
-});
+router.use('/states', statesRouter);
 
 module.exports = router;
